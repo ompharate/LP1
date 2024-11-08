01e01f3002628dcce7304faf55302cf82c25c3cc
om <ompharate31@gmail.com>
2024-11-08 08:40:55 +0530

    add sjf

--- SPOS/SJFScheduling.java
+++ SPOS/SJFScheduling.java
@@ -3,10 +3,11 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Process {
-    int id;
-    int burstTime;
-    int waitingTime;
-    int turnAroundTime;
+    int id;            
+    int burstTime;     
+    int waitingTime;   
+    int turnAroundTime; 
+    int completionTime; 
 
     Process(int id, int burstTime) {
         this.id = id;
@@ -29,31 +30,41 @@ public class SJFScheduling {
             processes[i] = new Process(i + 1, burstTime);
         }
 
+      
         Arrays.sort(processes, Comparator.comparingInt(p -> p.burstTime));
 
-        int totalWaitingTime = 0, totalTurnAroundTime = 0;
+        int totalWaitingTime = 0, totalTurnAroundTime = 0, totalCompletionTime = 0;
         processes[0].waitingTime = 0;
         processes[0].turnAroundTime = processes[0].burstTime;
+        processes[0].completionTime = processes[0].burstTime; 
 
+      
         for (int i = 1; i < n; i++) {
             processes[i].waitingTime = processes[i - 1].waitingTime + processes[i - 1].burstTime;
             processes[i].turnAroundTime = processes[i].waitingTime + processes[i].burstTime;
+            processes[i].completionTime = processes[i].waitingTime + processes[i].burstTime;
 
             totalWaitingTime += processes[i].waitingTime;
             totalTurnAroundTime += processes[i].turnAroundTime;
+            totalCompletionTime += processes[i].completionTime;
         }
 
+        
         System.out.println("\nProcess Execution Order (SJF): ");
         for (Process process : processes) {
             System.out.println("Process " + process.id + ": Burst Time = " + process.burstTime
                     + ", Waiting Time = " + process.waitingTime
-                    + ", Turnaround Time = " + process.turnAroundTime);
+                    + ", Turnaround Time = " + process.turnAroundTime
+                    + ", Completion Time = " + process.completionTime);
         }
 
+    
         double avgWaitingTime = (double) totalWaitingTime / n;
         double avgTurnAroundTime = (double) totalTurnAroundTime / n;
+        double avgCompletionTime = (double) totalCompletionTime / n;
 
         System.out.println("\nAverage Waiting Time: " + avgWaitingTime);
         System.out.println("Average Turnaround Time: " + avgTurnAroundTime);
+        System.out.println("Average Completion Time: " + avgCompletionTime);
     }
 }
